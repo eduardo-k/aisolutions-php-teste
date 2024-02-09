0529c60f0ee5b3db14b614a6e31b9631b2e6a1d8
Eduardo Kopper <ekopper@gmail.com>
2024-02-09 14:33:13 -0300

    task 1: migration checks if table exists before creating it

--- database/migrations/2023_03_28_172350_create_categories_table.php
+++ database/migrations/2023_03_28_172350_create_categories_table.php
@@ -11,11 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('name', 20);
-        });
+        if (!Schema::hasTable('categories')) {
+            Schema::create('categories', function (Blueprint $table) {
+                $table->id();
+                $table->timestamps();
+                $table->string('name', 20);
+            });
+        }
     }
 
     /**
